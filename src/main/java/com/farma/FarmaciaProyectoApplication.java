8e9bfc343248b265edad07dc6e4bf185f45099b7
francisco granados <francisco.granados@tiendasneto.com-at-768971106385>
2023-11-28 16:22:10 -0600

    Se inicio el proyecto farmacia con nuevos archivos configurados

---
+++ src/main/java/com/farma/FarmaciaProyectoApplication.java
@@ -0,0 +1,18 @@
+package com.farma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**+
+ *
+ * Clase principal que maneja todo el proyecto mas bien ejecuta todo spring
+ */
+@SpringBootApplication
+public class FarmaciaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaProyectoApplication.class, args);
+	}
+
+}
