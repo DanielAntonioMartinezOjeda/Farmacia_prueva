c3eb2f7c3a48aac5803f50aa0ed46d3c703b7437
francisco granados <francisco.granados@tiendasneto.com-at-768971106385>
2023-11-28 16:58:54 -0600

    se agrego dependencia en el pom de jpa SE CONFIGURO EL PROPPERTIES Y SE AGREGO UNA BASE DE DATOS TEMPORAL TAMBIEN DE MAPPEO UNAS CLASES

--- src/main/java/com/farma/Model/Role.java
+++ src/main/java/com/farma/Model/Role.java
@@ -1,9 +1,14 @@
 package com.farma.Model;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "roles")
 public class Role {
 
-
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
